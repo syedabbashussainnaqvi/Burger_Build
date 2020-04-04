44bc2213af4bc217aa9755ba67562eefd3948387
Abbas Naqvi <snaqvi.bscs16seecs@seecs.edu.pk>
2020-04-04 18:01:12 +0500

    changesDone

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
-const Burger = props => {
+const Burger = (props) => {
   const keys = Object.keys(props.ingredients);
 
   const showIngredients = keys.map((key, indexes) => {
